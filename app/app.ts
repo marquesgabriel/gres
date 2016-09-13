b09d22becd4af94d753c62881584dc96c4737980
marquesgabriel <gabrielf.pmarques@gmail.com>
2016-09-13 09:17:41 -0300

    created about page

--- app/app.ts
+++ app/app.ts
@@ -4,6 +4,7 @@ import { StatusBar } from 'ionic-native';
 
 import { Page1 } from './pages/page1/page1';
 import { Page2 } from './pages/page2/page2';
+import { About } from './pages/about/about';
 
 @Component({
   templateUrl: 'build/app.html'
@@ -21,7 +22,8 @@ class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Page uno', component: Page1 },
-      { title: 'Page dos', component: Page2 }
+      { title: 'Page dos', component: Page2 },
+      { title: 'Sobre', component: About }
     ];
 
   }
