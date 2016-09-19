bdb67fc423286cbb1126b56a1cd9ff33934f12b9
marquesgabriel <gabrielf.pmarques@gmail.com>
2016-09-19 17:46:40 -0300

    fix calc fat male

--- app/pages/three-male/three-male.ts
+++ app/pages/three-male/three-male.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController, Slides } from 'ionic-angular';
+import {PrevNext} from '../../components/prev-next/prev-next';
 
 @Component({
   templateUrl: 'build/pages/three-male/three-male.html',
@@ -7,6 +8,18 @@ import { NavController, Slides } from 'ionic-angular';
 })
 export class ThreeMale {
   @ViewChild('sequence') slides: Slides;
+  pt: number;
+  ab: number;
+  cx: number;
+  height: number;
+  weight: number;
+  age: number;
+
+  imc:string;
+  fat:string;
+  ga:string;
+  mcm:string;
+  pce:string;
 
   constructor(private navCtrl: NavController) {}
 
@@ -16,18 +29,48 @@ export class ThreeMale {
   goToPreviousSlide(){
     this.slides.slidePrev(300, false);
   };
-  calcImc(weight, height){
-    return (weight/(height*height)).toFixed(2);
+  private calcImc(){
+    return (this.weight/(this.height*this.height)).toFixed(2);
   };
-  calcFat(pt, ab, cx, age){
-    let dc = 1.109380-
-      (0.0008267*(pt+ab+cx))+
-      (0.0000016*((pt+ab+cx)*(pt+ab+cx)))-
-      (0.0002574*age);
-    return Math.abs(((4.95/dc)-4.5)*100).toFixed(2);
+  private calcFat(){
+    let pt = Number(this.pt);
+    let ab = Number(this.ab);
+    let cx = Number(this.cx);
+    let age = Number(this.age);
+
+    let sum = pt+ab+cx;
+    let pow = sum*sum;
+
+    let dc = 1.109380-(0.0008267*sum)+(0.0000016*pow)-(0.0002574*age);
+    return (Math.abs(((4.95/dc)-4.5)*100)).toFixed(2);
   };
-  banana(event){
+  private calcGa(){
+    let weight = this.weight;
+    let fat = Number(this.calcFat());
+    return (weight*(fat/100)).toFixed(2);
+  }
+  private calcMcm(){
+    let weight = this.weight;
+    let ga = Number(this.calcGa());
+    return (weight-ga).toFixed(2);
+  }
+  private calcPce(){
+    let fat = Number(this.calcFat());
+    let mcm = Number(this.calcMcm());
+    return (mcm/(1-fat)).toFixed(2)
+  }
+
+  enter(event){
     if(event.keyCode===13)
       this.goToNextSlide();
   }
+  // calc(){
+  //   this.imc = this.calcImc();
+  //   this.fat = this.calcFat();
+  //   this.ga = this.calcGa();
+  //   this.mcm = this.calcMcm();
+  //   // this.pce = this.calcPce();
+  //
+  //   this.goToNextSlide();
+  // }
 }
